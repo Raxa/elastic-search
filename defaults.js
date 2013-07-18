b9c974b9711ed76bf121a4b2c156e53a5f1c4a0c
invercity <invercity@invercity-K50ID>
2013-07-19 01:20:58 +0300

    added all entities

---
+++ defaults.js
@@ -0,0 +1,16 @@
+var Module = require('./lib/module').Module;
+
+var module = new Module();
+
+config.set('indexpateint',false);
+config.set('indexperson',false);
+config.set('indexprovider',false);
+config.set('indexdrug',false);
+config.set('indexlocation',false);
+config.set('indexconcept',false);
+config.set('indexencounter',false);
+config.set('indexorder',false);
+config.set('indexobs',false);
+
+console.log('config set do default');
+
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 // imports
 var Module = require('./lib/module').Module;
 var ES = require('./lib/searchserver').SearchServer;
+// import entities
 var Person = require('./lib/person').Person;
+var Patient = require('./lib/patient').Patient;
+var Encounter = require('./lib/encounter').Encounter;
+var Provider = require('./lib/provider').Provider;
+var Location = require('./lib/location').Location;
+var Concept = require('./lib/concept').Concept;
+var Drug = require('./lib/drug').Drug;
+var Obs = require('./lib/obs').Obs;
+var Order = require('./lib/order').Order;
 
 // create ES server
 var module = new Module();
 var es = new ES(module);
 // add required entities
 es.addEntity(new Person(module));
+es.addEntity(new Patient(module));
+es.addEntity(new Provider(module));
+es.addEntity(new Encounter(module));
+es.addEntity(new Location(module));
+es.addEntity(new Drug(module));
+es.addEntity(new Obs(module));
+es.addEntity(new Order(module));
+es.addEntity(new Concept(module));
 
-module.http.createServer(function(request, response) {
+var time = new Date();
+
+es.index(function(){console.log('indexing done :' + (new Date() - time));});
+
+http.createServer(function(request, response) {
     if (request.method === "POST") {
         // data buffer
         var data = '';
@@ -28,11 +49,11 @@ module.http.createServer(function(request, response) {
             });
         });
     } else {
-        var filePath = module.path.join('.', request.url);
+        var filePath = path.join('.', request.url);
         //checking includes
-        module.path.exists(filePath, function(exists) {
+        path.exists(filePath, function(exists) {
             if (exists) {
-                var extname = module.path.extname(filePath);
+                var extname = path.extname(filePath);
                 var contentType = 'text/html';
                 switch (extname) {
                 case '.js':
@@ -42,7 +63,7 @@ module.http.createServer(function(request, response) {
                     contentType = 'text/css';
                     break;
                 }
-                module.fs.readFile(filePath, function(error, content) {
+                fs.readFile(filePath, function(error, content) {
                     if (error) {
                         response.writeHead(500);
                         response.end();
